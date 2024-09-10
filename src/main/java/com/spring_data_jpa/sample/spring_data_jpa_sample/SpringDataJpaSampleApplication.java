26a5dc0e9be3351d4f59ca245332fa73f912ad47
Arun Gandhi <arungandhi@Aruns-MacBook-Air.local>
2024-09-10 09:11:59 +0530

    init data jpa project

---
+++ src/main/java/com/spring_data_jpa/sample/spring_data_jpa_sample/SpringDataJpaSampleApplication.java
@@ -0,0 +1,13 @@
+package com.spring_data_jpa.sample.spring_data_jpa_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaSampleApplication.class, args);
+	}
+
+}
